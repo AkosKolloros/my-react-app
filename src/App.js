d8e39682e83d9a1149e310686cf617d487d936b2
Akos Kolloros <kollorosa@gmail.com>
2024-07-27 13:41:49 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { MyButton } from "./MyButton";
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. s
         </p>
         <a
           className="App-link"
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <MyButton />
       </header>
     </div>
   );
---
+++ src/MyButton.js
@@ -0,0 +1,9 @@
+import Button from 'react-bootstrap/Button';
+
+export function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
